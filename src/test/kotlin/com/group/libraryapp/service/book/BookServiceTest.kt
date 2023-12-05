cd70ae93bb7d317082bb1c52a30af9a58e49e7be
Ho-Ya-Ho <rkdgh1056@gmail.com>
2023-12-05 17:37:06 +0900

    리팩토링 전 test code 추가

---
+++ src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
@@ -0,0 +1,86 @@
+package com.group.libraryapp.service.book
+
+import com.group.libraryapp.domain.book.Book
+import com.group.libraryapp.domain.book.BookRepository
+import com.group.libraryapp.domain.user.User
+import com.group.libraryapp.domain.user.UserRepository
+import com.group.libraryapp.domain.user.loanhistory.UserLoanHistory
+import com.group.libraryapp.domain.user.loanhistory.UserLoanHistoryRepository
+import com.group.libraryapp.dto.book.request.BookLoanRequest
+import com.group.libraryapp.dto.book.request.BookRequest
+import com.group.libraryapp.dto.book.request.BookReturnRequest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookServiceTest @Autowired constructor(
+    private val bookService: BookService,
+    private val bookRepository: BookRepository,
+    private val userRepository: UserRepository,
+    private val userLoanHistoryRepository: UserLoanHistoryRepository,
+) {
+
+    @AfterEach
+    fun clean() {
+        bookRepository.deleteAll()
+        userRepository.deleteAll()
+    }
+
+    @Test
+    fun `책 저장이 정상 동작 한다`() {
+        val request = BookRequest("book1")
+
+        bookService.saveBook(request)
+
+        val results = bookRepository.findAll()
+        assertThat(results).hasSize(1)
+        assertThat(results[0].name).isEqualTo("book1")
+    }
+
+    @Test
+    fun `책 대출이 정상 동작 한다`() {
+        bookRepository.save(Book("book1"))
+        val savedUser = userRepository.save(User("user1", 20))
+        val request = BookLoanRequest("user1", "book1")
+
+        bookService.loanBook(request)
+
+        val results = userLoanHistoryRepository.findAll()
+        assertThat(results).hasSize(1)
+        assertThat(results[0].bookName).isEqualTo("book1")
+        assertThat(results[0].user.id).isEqualTo(savedUser.id)
+        assertThat(results[0].isReturn).isFalse
+    }
+
+    @Test
+    fun `책이 진짜 대출되어 있다면, 신규 대출이 실패한다`() {
+        bookRepository.save(Book("book1"))
+        val savedUser = userRepository.save(User("user1", 20))
+        userLoanHistoryRepository.save(UserLoanHistory(savedUser, "book1", false))
+        val request = BookLoanRequest("user1", "book1")
+
+        val message = assertThrows<IllegalArgumentException> {
+            bookService.loanBook(request)
+        }.message
+
+        assertThat(message).isEqualTo("진작 대출되어 있는 책입니다")
+    }
+
+    @Test
+    fun `책 반납이 정상 동작한다`() {
+        val savedUser = userRepository.save(User("user1", 20))
+        userLoanHistoryRepository.save(UserLoanHistory(savedUser, "book1", false))
+        val request = BookReturnRequest("user1", "book1")
+
+        bookService.returnBook(request)
+
+        val results = userLoanHistoryRepository.findAll()
+        assertThat(results).hasSize(1)
+        assertThat(results[0].isReturn).isTrue
+    }
+
+}
---
+++ src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
@@ -0,0 +1,69 @@
+package com.group.libraryapp.service.user
+
+import com.group.libraryapp.domain.user.User
+import com.group.libraryapp.domain.user.UserRepository
+import com.group.libraryapp.dto.user.request.UserCreateRequest
+import com.group.libraryapp.dto.user.request.UserUpdateRequest
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserServiceTest @Autowired constructor(
+    private val userService: UserService,
+    private val userRepository: UserRepository
+){
+
+    @AfterEach
+    fun cleanUp() {
+        userRepository.deleteAll()
+    }
+
+    @Test
+    fun `유저 저장이 정상 동작 한다`() {
+        val request = UserCreateRequest("user1", null)
+
+        userService.saveUser(request)
+
+        val results = userRepository.findAll()
+        assertThat(results.size).isEqualTo(1)
+        assertThat(results[0].name).isEqualTo("user1")
+        assertThat(results[0].age).isNull()
+    }
+
+    @Test
+    fun `유저 조회가 정상 동작 한다`() {
+        userRepository.saveAll(listOf(
+            User("user1", 20),
+            User("user2", null),
+        ))
+
+        val users = userService.getUsers()
+
+        assertThat(users.size).isEqualTo(2)
+        assertThat(users).extracting("name").containsExactly("user1", "user2")
+        assertThat(users).extracting("age").containsExactly(20, null)
+    }
+
+    @Test
+    fun `유저 업데이트가 정상 동작 한다`() {
+        val savedUser = userRepository.save(User("A", 20))
+        val request = UserUpdateRequest(savedUser.id, "B")
+
+        userService.updateUserName(request)
+
+        val result = userRepository.findAll()[0]
+        assertThat(result.name).isEqualTo("B")
+    }
+
+    @Test
+    fun `유저 삭제가 정상 동작 한다`() {
+        userRepository.save(User("A", 20))
+        userService.deleteUser("A")
+
+        val result = userRepository.findAll()
+        assertThat(result).isEmpty()
+    }
+}
