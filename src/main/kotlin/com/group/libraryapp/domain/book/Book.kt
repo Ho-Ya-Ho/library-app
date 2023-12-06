d8089222f767c99dbfaa05c3e2cfae1397f9205b
Ho-Ya-Ho <rkdgh1056@gmail.com>
2023-12-06 12:15:37 +0900

    (refactoring) javaBook -> KotlinBook

---
+++ src/main/kotlin/com/group/libraryapp/domain/book/Book.kt
@@ -0,0 +1,19 @@
+package com.group.libraryapp.domain.book
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType.IDENTITY
+import javax.persistence.Id
+
+@Entity
+class Book(
+    val name: String,
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    val id: Long? = null,
+) {
+    init {
+        require(name.isNotBlank()) { "이름은 비어 있을 수 없습니다" }
+    }
+}
